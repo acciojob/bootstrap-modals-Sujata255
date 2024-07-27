b911939828e6f919c56a7a3aea4501019a994ae9
Sujata255 <das19sujata@gmail.com>
2024-07-27 14:56:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your code here
+window.onload = function() {
+  var modal = document.getElementById('newton-modal');
+  var btn = document.getElementById('open-modal');
+
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+}
